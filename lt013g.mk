b44a51146d6d5c7e60448450e819cb72016ef669
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2013-12-04 12:07:23 +0700

    lt013g.mk: Enable tablet mode

--- lt013g.mk
+++ lt013g.mk
@@ -65,8 +65,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
+PRODUCT_CHARACTERISTICS := tablet
 
 $(call inherit-product-if-exists, vendor/samsung/lt013g/lt013g-vendor.mk)
