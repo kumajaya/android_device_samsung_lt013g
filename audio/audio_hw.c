0993011553733951c20da26da58da35c3b91bb23
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2014-01-04 20:46:42 +0700

    Change device spesific audio route, rename audio module

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -3114,7 +3114,7 @@ struct audio_module HAL_MODULE_INFO_SYM = {
         .module_api_version = AUDIO_MODULE_API_VERSION_0_1,
         .hal_api_version = HARDWARE_HAL_API_VERSION,
         .id = AUDIO_HARDWARE_MODULE_ID,
-        .name = "M0 audio HW HAL",
+        .name = "LT01 audio HW HAL",
         .author = "The CyanogenMod Project",
         .methods = &hal_module_methods,
     },
