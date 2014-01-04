164ea69cf63f5fbc8256d7bb837cf6a2e339b630
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2014-01-05 00:01:57 +0700

    Disable button backlight for now

--- common.mk
+++ common.mk
@@ -50,6 +50,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
 
+# Button backlight disabler
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/81backlight:system/etc/init.d/81backlight
+
 PRODUCT_CHARACTERISTICS := tablet
 
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
